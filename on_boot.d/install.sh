b1d82dec83632e95ce7d233b1e0400110f00b0ab
Jason Bray <me@jasonbray.us>
2024-02-21 19:58:45 -0800

    Update install.sh

--- on_boot.d/install.sh
+++ on_boot.d/install.sh
@@ -26,7 +26,7 @@ case "$(ubnt-device-info firmware || true)" in
     ;;
 esac
 
-GH_REPO="renedis/ubnt-auto-fan-speed"
+GH_REPO="bliptec/ubnt-auto-fan-speed"
 GH_URL_BASE="https://raw.githubusercontent.com/${GH_REPO}/HEAD"
 
 if [ $INSTALL_TYPE -eq 1 ]; then
